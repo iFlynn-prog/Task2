7b6b500d1772f37d537785885e74cebfce92083c
iFlynn-prog <minibikk@gmail.com>
2022-09-13 17:15:58 +0300

    Bubble sort

--- src/Task2.java
+++ src/Task2.java
@@ -1,21 +1,21 @@
+import java.util.Arrays;
+
 public class Task2 {
     public static void main(String[] args){
 
         int[] numbers = new int[] {54, 7, -41, 2, 4, 2, 89, 33, -5, 12};
-        for (int i = numbers.length - 1; i > 0; i--)
+        for (int i = 0; i < numbers.length; i++) //Инициализация массива
         {
-        for (int j = 0; j < 1; j++)
+        for (int j = 0; j < numbers.length - 1; j++) //Внутренний цикл который сравнивает до предыдущего элемента
         {
-            if(numbers[i] > numbers[j + 1])
+            if(numbers[j] > numbers[j + 1]) //Сравнение первой ячейки массива со следующей
             {
-                int tmp = numbers[j];
-                numbers[j] = numbers[j + 1];
-                numbers[j] = tmp;
+                int tmp = numbers[j];           // Перенос массива во временну переменную
+                numbers[j] = numbers[j + 1];    //Пример как перелить стакан воды и стакан молока
+                numbers[j + 1] = tmp;           //Для этого необходим 3 временный стакан (tmp)
             }
-
         }
-            System.out.print(numbers[i]);
         }
-
+        System.out.print(Arrays.toString(numbers)); //Вывод массива после цикла
     }
 }
